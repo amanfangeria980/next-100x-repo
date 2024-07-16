9f9d5dc46dcb1e4ac00627b9acb5d83500552adb
Aman Fangeria <aman.fangeria@geekyants.com>
2024-07-16 19:37:38 +0530

    frontend for sign up and backend for signup done

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,18 @@
+import { NextRequest } from "next/server";
+
 export function GET() {
     return Response.json({
         email: "amanjifangeria@gmail.com",
         name: "Aman Fangeria",
+    });
+}
+
+export async function POST(req: NextRequest) {
+    // extract the body
+    const body = await req.json();
+    console.log(body);
+    // store the body in the db
+    return Response.json({
+        message: "You are logged in.",
     });
 }
